46aa8fdc322a030148513d1acbc0ebc9efbe8c2f
clz <fadinglight9291117@126.com>
2021-08-05 12:28:46 +0800

    find some error

--- data/faceset.py
+++ data/faceset.py
@@ -1,7 +1,13 @@
 from pathlib import Path
 import json
+from easydict import EasyDict as edict
 
 import cv2
+
+
+def get_crop(img, box):
+    return img[box[1]: box[3], box[0]: box[2]]
+
 
 faceset_path = Path('faceset')
 images_path = faceset_path / 'images'
@@ -16,5 +22,27 @@ label_path = list(images_path.glob('*.json'))[0]
 with label_path.open() as f:
     labels = json.load(f)
 
+infos = []
+
 for label in labels:
-    im = cv2.imread()
+    label = edict(label)
+    imgL_name = label.img_name
+    img_path = imgL_path / imgL_name
+    imgL = cv2.imread(img_path.__str__())
+    boxes = label.boxes
+    for i, box in enumerate(boxes):
+        imgS = get_crop(imgL, box)
+        imgS_name = f'{Path(imgL_name).stem}_{i}.jpg'
+        save_path = imgS_path / imgS_name
+        try:
+            cv2.imwrite(save_path.__str__(), imgS)
+        except Exception:
+            ...
+        info = {
+            'imgL_name': imgL_name,
+            'imgS_name': imgS_name,
+            'box': box,
+        }
+        infos.append(info)
+with (faceset_path / 'img_info.json').open('w', encoding='utf-8') as f:
+    json.dump(infos, f)
